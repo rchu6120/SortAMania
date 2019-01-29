8381bbf089be34e4122a6cb3c93617aafb3f6325
BT_1N3_28 <yibarra0090@bths.edu>
2019-01-29 14:12:32 -0500

    123

---
+++ src/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+    SortCompetition team1 = new TeamSortCompetition();
+    int[] randIntArr = randomIntsArr(10000);
+    String[] randStringArr = randomStringArr(10000,5);
+    System.out.println("Unsorted");
+    printArr(randIntArr);
+    long time = System.currentTimeMillis();
+    int median = team1.ch
+}
