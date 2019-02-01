fac91daf6a9296eb048b1f7e5dd3f75f790e2145
BT_1N3_29 <hkaur0745@bths.edu>
2019-02-01 14:37:12 -0500

    challenge one median finder added

--- src/Team12SortCompetition.java
+++ src/Team12SortCompetition.java
@@ -1,7 +1,15 @@
 public class Team12SortCompetition extends SortCompetition {
     @Override
-    public int challengeOne(int[] arr) {
-        return 0;
+    public int challengeOne(int[] arr)
+    {
+        // to find the median
+        int middle = arr.length/2;
+        int median = 0; //declare variable
+        if (arr.length%2 == 1)
+            median = arr[middle];
+        else
+            median = (arr[middle-1] + arr[middle]) / 2;
+        return median;
     }
 
     @Override
