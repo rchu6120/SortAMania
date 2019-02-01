f8bfbce13927cb78b9aee090b5363c1bd0c3ebca
BT_1N3_30 <zchen5143@brths>
2019-02-01 14:22:40 -0500

    INITIASFLD COMMIT

---
+++ src/Team11SortCompetition.java
@@ -0,0 +1,31 @@
+public class Team11SortCompetition extends SortCompetition {
+    @Override
+    public int challengeOne(int[] arr) {
+        return 0;
+    }
+
+    @Override
+    public int challengeTwo(String[] arr, String query) {
+        return 0;
+    }
+
+    @Override
+    public int challengeThree(int[] arr) {
+        return 0;
+    }
+
+    @Override
+    public int challengeFour(int[][] arr) {
+        return 0;
+    }
+
+    @Override
+    public int challengeFive(Comparable[] arr, Comparable query) {
+        return 0;
+    }
+
+    @Override
+    public String greeting() {
+        return "Ari is not gonna win. We are Team 11 BTW \n Allen and Johnathan";
+    }
+}
