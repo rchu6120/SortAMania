540602c20829cc7508960c0a5f5db4e081252f17
BT_1N3_16 <bwu3112@bths.edu>
2019-02-01 14:14:40 -0500

    a

---
+++ src/Team7SortCompetition.java
@@ -0,0 +1,25 @@
+public class Team7SortCompetition extends SortCompetition {
+    public int challengeOne(int[] arr) {
+        return 0;
+    }
+
+    public int challengeTwo(String[] arr, String query) {
+        return 0;
+    }
+
+    public int challengeThree(int[] arr) {
+        return 0;
+    }
+
+    public int challengeFour(int[][] arr) {
+        return 0;
+    }
+
+    public int challengeFive(Comparable[] arr, Comparable query) {
+        return 0;
+    }
+
+    public String greeting() {
+        return null;
+    }
+}
