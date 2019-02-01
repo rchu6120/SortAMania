0c5ad1a3ca9ee9e701a3ae31d959c5568d0afcf4
BT_1N3_29 <hkaur0745@bths.edu>
2019-02-01 14:16:43 -0500

    greeting

---
+++ src/Team12SortCompetition.java
@@ -0,0 +1,31 @@
+public class Team12SortCompetition extends SortCompetition {
+    @Override
+    public int challengeOne(int[] arr) {
+        return 0;
+    }
+
+    @Override
+    public int challengeTwo(String[] arr, String query) {
+        return 0;
+    }
+
+    @Override
+    public int challengeThree(int[] arr) {
+        return 0;
+    }
+
+    @Override
+    public int challengeFour(int[][] arr) {
+        return 0;
+    }
+
+    @Override
+    public int challengeFive(Comparable[] arr, Comparable query) {
+        return 0;
+    }
+
+    @Override
+    public String greeting() {
+        return "Hello! We are team 12 :)";
+    }
+}
