ed4162e28b3576d2fdc5bf2b0b4ac7e34cb1f042
BT_1N3_26 <mister.p730@gmail.com>
2019-02-01 14:44:43 -0500

    merge

--- src/Sorting_Algorithims.java
+++ src/Sorting_Algorithims.java
@@ -1,7 +1,45 @@
 public class Sorting_Algorithims
 {
-    public static void merge(int[] arr, int from, int mid,int[] temp)
+    public static void merge(int[] arr, int from, int mid, int to, int[] temp)
     {
+        int x = from;
+        int y = mid+1;
+        int z = from;
 
+        while(x<= mid && y <= to)
+        {
+            if(arr[x]<arr[y])
+            {
+                temp[z] = arr[x];
+                x++;
+            }
+            else
+            {
+                temp[z] = arr[y];
+                y++;
+            }
+            z++;
+        }
+
+        while (x <= mid)
+        {
+            temp[z] = arr[x];
+            z++;
+            x++;
+        }
+        while (y<= to)
+        {
+            temp[z] = arr[y];
+            z++;
+            y++;
+        }
+    }
+    private static void mergeSortHelper(int[] arr, int from, int to, int[] temp)
+    {
+        if(from<to)
+        {
+            int middle = (from + to)/2;
+            mergeSortHelper(arr,from,m)
+        }
     }
 }
