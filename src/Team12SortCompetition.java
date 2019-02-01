39d51d2f8fb8b74b7ea9411d13cc7afb938f3e33
BT_1N3_29 <hkaur0745@bths.edu>
2019-02-01 14:43:04 -0500

    challnege one edits

--- src/Team12SortCompetition.java
+++ src/Team12SortCompetition.java
@@ -2,6 +2,13 @@ public class Team12SortCompetition extends SortCompetition {
     @Override
     public int challengeOne(int[] arr)
     {
+        //makes an array of 1000 random integers
+        public static int[] randIntArr(int count)
+        {
+        int[] num = new int[count];
+        for(int i = 0; i < count; i++){
+            num[i] = (int)(Math.random()*10001);
+        }
         // to find the median
         int middle = arr.length/2;
         int median = 0; //declare variable
