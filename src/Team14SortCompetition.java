7333f3a1fbd38e514fb39b5cbf3c5e9782bf93d0
BT_1N3_24 <ljaoutakas8934@bths.edu>
2019-02-01 14:17:48 -0500

    Chen Brian

---
+++ src/Team14SortCompetition.java
@@ -0,0 +1,39 @@
+public class Team14SortCompetition extends SortCompetition
+{
+
+    @Override
+    public int challengeOne(int[] arr)
+    {
+        return 0;
+    }
+
+    @Override
+    public int challengeTwo(String[] arr, String query)
+    {
+        return 0;
+    }
+
+    @Override
+    public int challengeThree(int[] arr)
+    {
+        return 0;
+    }
+
+    @Override
+    public int challengeFour(int[][] arr)
+    {
+        return 0;
+    }
+
+    @Override
+    public int challengeFive(Comparable[] arr, Comparable query)
+    {
+        return 0;
+    }
+
+    @Override
+    public String greeting()
+    {
+        return "We are Team14, If we win, please send V-bucks to chenBrian@fortnite.edu";
+    }
+}
