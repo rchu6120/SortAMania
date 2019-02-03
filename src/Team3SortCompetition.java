285a0477901db1e74d7bbcadefbdaa993e41bdc1
danazargarova <dzargarova6105@bths.edu>
2019-02-02 20:10:05 -0500

    greeting and our class teehee owo

---
+++ src/Team3SortCompetition.java
@@ -0,0 +1,27 @@
+
+public class Team3SortCompetition extends SortCompetition {
+
+    public int challengeOne(int[] arr){
+
+    }
+
+    public int challengeTwo(String[] arr, String query){
+
+    }
+
+    public int challengeThree(int[] arr){
+
+    }
+
+    public int challengeFour(int[][] arr){
+
+    }
+
+    public int challengeFive(Comparable[] arr, Comparable query){
+
+    }
+
+    public String greeting(){
+        return "Beware Team 3!! OwO.";
+    }
+}
