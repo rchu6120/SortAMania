d874e15329aac6da7dee0dceb46c33fa60761f7e
aclarke9691 <aclarke9691>
2019-02-04 15:12:31 -0500

    it's ok

--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,22 @@
 public class Runner {
+
+    public static void main(String [] args)
+    {
+        int[] numbers = new int[10000];
+        for (int i = 0; i <10000; i++)
+        {
+            numbers[i]=(int) (Math.random()*10000 +1);
+        }
+            int a=0;
+
+        //while (a<20)
+        //{
+            //System.out.println(numbers[a]);
+           // a=a+1;
+       //}
+        System.out.println(Team2SortCompetition.challengeOne(numbers));
+    }
+
+
+
 }
