19edda570ec825187227f1580e02aeffd68ec98c
BT_1N3_06 <arafi6624@bths.edu>
2019-02-04 14:39:06 -0500

    a

--- src/Team2SortCompetition.java
+++ src/Team2SortCompetition.java
@@ -3,6 +3,7 @@ public class Team2SortCompetition extends SortCompetition {
     public int challengeOne(int[] arr)
     {
 
+
        int median= arr[4999];
        return median;
     }
@@ -73,7 +74,28 @@ public class Team2SortCompetition extends SortCompetition {
             i++;
             k++;
         }
+    }
 
+    public class BubbleSort {
+        public static void swap(int[] arr, int i, int j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        public static void BubbleSort(int[] arr) {
+            int[] arrTemp = arr;
+            int swaps = -1;
+            while (swaps != 0) {
+                swaps = 0;
+                for (int i = 0; i < arr.length - 1; i++) {
+                    if (arr[i] > arr[i + 1]) {
+                        swap(arr, i, i + 1);
+                        swaps++;
+                    }
+                }
+            }
+        }
     }
 
 }
