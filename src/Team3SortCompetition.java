a71a06d9a83a8247572dc2bdbd51dc9f12763e9b
dana <dzargarova6105@bths.edu>
2019-02-04 14:44:40 -0500

    teehee owo

--- src/Team3SortCompetition.java
+++ src/Team3SortCompetition.java
@@ -1,23 +1,25 @@
 
 public class Team3SortCompetition extends SortCompetition {
 
-    public int challengeOne(int[] arr){
+    public int challengeOne(int[] arr){ //array pf 10,000 random ints. sort list and return median
+
 
     }
 
-    public int challengeTwo(String[] arr, String query){
+    public int challengeTwo(String[] arr, String query){ //10,000 random strings length 5. sort list. determine if it contains a given string. return the index of the first instance or -1 if not found
 
     }
 
-    public int challengeThree(int[] arr){
+    public int challengeThree(int[] arr){ //mostly sorted array of 100,000 ints. sort list and return median
 
     }
 
-    public int challengeFour(int[][] arr){
+    public int challengeFour(int[][] arr){ //two dimensional array. 1,000 by 1,000 with random integers between 0 and 10,000
 
     }
 
-    public int challengeFive(Comparable[] arr, Comparable query){
+    public int challengeFive(Comparable[] arr, Comparable query){ //array of 10,000 objects that implement comparable. sort using compareTo method. return position of the object or -1.
+                                                                    //you must use a stable sort
 
     }
 
