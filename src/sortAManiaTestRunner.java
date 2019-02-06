84e8c42af17213b804b9355e795f17bf45cfbb96
BT_1N3_19 <jlin3392@bths.edu>
2019-02-06 14:44:53 -0500

    challenge 3 complete 2.0

--- src/sortAManiaTestRunner.java
+++ src/sortAManiaTestRunner.java
@@ -3,22 +3,21 @@ public class sortAManiaTestRunner extends Team8SortCompetition {
     public static void main(String[] args) {
 
         SortCompetition team1 = new Team8SortCompetition();
-        int[] randIntArr = randIntsArr(14);
-        int[] mostlySorted = {2003, 2567, 2679, 3567, 4433, 5421, 5331, 4000};
+        int[] randIntArr = randIntsArr(10000);
         //String[] randStringArr = randomStringArr(10000, 5);
 
         System.out.println("Unsorted");
-        printIntArr(mostlySorted);
+        printIntArr(randIntArr);
 
         long time = System.currentTimeMillis();
-        //int median = team1.challengeOne(randIntArr);
-        int median2 = team1.challengeThree(mostlySorted);
+        int median = team1.challengeOne(randIntArr);
+        //int median2 = team1.challengeThree(mostlySorted);
         time = System.currentTimeMillis() - time;
         System.out.println("Challenge One Time Taken: " + time * 0.001 + "Seconds");
-        //System.out.println("Median equals: " + median);
-        System.out.println("Median equals: " + median2);
+        System.out.println("Median equals: " + median);
+        //System.out.println("Median equals: " + median2);
 
         System.out.println("Sorted");
-        printIntArr(mostlySorted);
+        printIntArr(randIntArr);
     }
 }
