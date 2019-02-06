ad1268bac978000687eb0702f898ef96ce40412d
BT_1N3_24 <ljaoutakas8934@bths.edu>
2019-02-06 14:27:25 -0500

    Brian Chen

--- src/Team14SortCompetition.java
+++ src/Team14SortCompetition.java
@@ -302,7 +302,46 @@ public class Team14SortCompetition extends SortCompetition
         }
     }
 
-    public void merge ()
+    public void merge (Comparable [] elements, int from, int mid, int to, Comparable []temp)
+    {
+        int i = from;
+        int j = mid + 1;
+        int k = from;
+
+        while (i <= mid && j <= to)
+        {
+            if (elements[i].compareTo(elements[j])<0)
+            {
+                temp [k] = elements[i];
+                i++;
+            }
+            else
+            {
+                temp[k] = elements[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i <= mid)
+        {
+            temp [k] = elements[i];
+            i++;
+            k++;
+        }
+
+        while (j <= to)
+        {
+            temp [k] = elements[j];
+            j++;
+            k++;
+        }
+
+        for (k = from; k <= to; k++)
+        {
+            elements[k] = temp [k];
+        }
+    }
 
 
 }
