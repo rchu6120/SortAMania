2b9b40d86a81f7535d67a516ac540d81c324ca7a
BT_1N3_18 <jtan5662@bths.edu>
2019-02-07 14:21:00 -0500

    jason-rename

--- src/Runner.java
+++ src/Runner.java
@@ -6,23 +6,23 @@ public class Runner {
         SortCompetition team7= new Team7SortCompetition();
 
         //Generates random int and Strings
-        int[] challengeOne= Team7SortCompetition.randIntArr(10000);
-        String[] randStringArr= Team7SortCompetition.randStringArr(10000,5);
+        int[] randIntArr1= Team7SortCompetition.randIntArr(10000);
+        String[] randStringArr2= Team7SortCompetition.randStringArr(10000,5);
 
         //Notifies user that the generated arrays are unsorted and prints out the unsorted arrays
-        System.out.println(Team7SortCompetition.printArr(challengeOne));
+        System.out.println(Team7SortCompetition.printArr(randIntArr1));
 
 
         //Prints time of ChallengeOne
         long time= System.currentTimeMillis();
-        int median= team7.challengeOne(challengeOne);
+        int median= team7.challengeOne(randIntArr1);
 
         time = System.currentTimeMillis() - time;
         System.out.println("Challenge One Time Taken: " + time * 0.001 + " Seconds");
         System.out.println("Median equals: " + median);
 
         //Prints out sorted Int Array
-        System.out.println(Team7SortCompetition.printArr(challengeOne));
+        System.out.println(Team7SortCompetition.printArr(randIntArr1));
         System.out.println("this is sorted");
 
 
