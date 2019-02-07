53a805979e72dbbc01b3494a3f25a013c195c2de
aclarke9691 <aclarke9691@bths.edu>
2019-02-07 14:45:01 -0500

    jkseab

--- src/Runner.java
+++ src/Runner.java
@@ -7,8 +7,23 @@ public class Runner {
         return arr;
     }
 
-    public static String[] randomStringArr(String arr){
-
+    public static String[] randomStringArr(int num, int length)
+    {
+        String[] arr= new String [num];
+        while (num>0)
+        {
+            int i=0;
+            String s="";
+            while (i<length)
+            {
+                char c= (char)((Math.random()*26)+97);
+                s=s+c;
+                i++;
+            }
+            num--;
+            arr[num]=s;
+        }
+        return arr;
     }
 
     public static void main (String[] args) {
