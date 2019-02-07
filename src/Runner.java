cca8de188d256076339d716f50fc6c5cf07f13bf
BT_1N3_28 <yibarra0090@bths.edu>
2019-02-07 14:34:06 -0500

    Runner almost complete!

--- src/Runner.java
+++ src/Runner.java
@@ -2,22 +2,67 @@
 public class Runner {
     public static void main(String [] args){
         SortCompetition team12 = new Team12SortCompetition();
-        int[] randInrArr = Team12SortCompetition.randomIntsArr(10000);
+        int[] randIntArr = Team12SortCompetition.randomIntsArr(10000);
         String [] randStringArr = Team12SortCompetition.randomStringArr(10000,5);
-
+            //CHALLENGE 1
         System.out.println("Unsorted");
-        Team12SortCompetition.printArr(randInrArr);
+        Team12SortCompetition.printArr(randIntArr);
 
         long time = System.currentTimeMillis();
-        int median = team12.challengeOne(randInrArr);
+        int median = team12.challengeOne(randIntArr);
         time = System.currentTimeMillis() - time;
         System.out.println("Challenge One Time Taken: " + time * 0.001 + " Seconds");
         System.out.println("Median equals: " + median);
 
         System.out.println("Sorted");
-        Team12SortCompetition.printArr(randInrArr);
+        Team12SortCompetition.printArr(randIntArr);
 
         System.out.println("__________________________________________________________________________________________________________________________________________________________________________________________________________________________");
+           //CHALLENGE 2
+        System.out.println("Unsorted");
+        Team12SortCompetition.printArr(randStringArr);
+
+        long time2 = System.currentTimeMillis();
+        int indexOfQuery = team12.challengeTwo(randStringArr,"query");
+        time2 = System.currentTimeMillis() - time2;
+        System.out.println("Challenge Two Time Taken: " + time * 0.001 + " Seconds");
+        System.out.println("Index equals: " + indexOfQuery);
+
+        System.out.println("Sorted");
+        Team12SortCompetition.printArr(randStringArr);
+
+        System.out.println("__________________________________________________________________________________________________________________________________________________________________________________________________________________________");
+            //CHALLENGE 3
+        int[] randIntArr2 = Team12SortCompetition.randomIntsArr(100000);
+        System.out.println("Unsorted");
+        Team12SortCompetition.printArr(randIntArr);
+
+        long time3 = System.currentTimeMillis();
+        int median3 = team12.challengeOne(randIntArr);
+        time = System.currentTimeMillis() - time;
+        System.out.println("Challenge One Time Taken: " + time * 0.001 + " Seconds");
+        System.out.println("Median equals: " + median);
+
+        System.out.println("Sorted");
+        Team12SortCompetition.printArr(randIntArr);
+
+        System.out.println("__________________________________________________________________________________________________________________________________________________________________________________________________________________________");
+            //CHALLENGE 4
+        System.out.println("Unsorted");
+        Team12SortCompetition.printArr(randIntArr);
+
+        long time = System.currentTimeMillis();
+        int median = team12.challengeOne(randIntArr);
+        time = System.currentTimeMillis() - time;
+        System.out.println("Challenge One Time Taken: " + time * 0.001 + " Seconds");
+        System.out.println("Median equals: " + median);
+
+        System.out.println("Sorted");
+        Team12SortCompetition.printArr(randIntArr);
+
+        System.out.println("__________________________________________________________________________________________________________________________________________________________________________________________________________________________");
+        //CHALLENGE 5
+
 
 
     }
