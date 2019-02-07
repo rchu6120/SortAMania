c37bb13b6746408dd29c958b980b246b690fefb3
BT_1N3_27 <sdfn>
2019-02-07 14:43:00 -0500

    I liek Turtls Runner almost finshed. Just need to make the random int method

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -1,6 +1,26 @@
-public class TestRunner
-{
-    public static void main(String[] args) {
+public class TestRunner {
+    public static String[] randomStringArr(int num,int length) {
+
+
+    }
+
+    public static int[] randomIntsArr(int length) {
+        int [] arr = new int[length];
+        for(int w = 0; w < arr.length;w++){
+            arr[w] = (int)(Math.random()*100000);
+        }
+        return arr;
+    }
+
+    public static void printArr(int [] arr) {
+        for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i]);
+
+        }
+        System.out.println();
+    }
+
+    public static void main (String[]args){
         SortCompetition team11 = new Team13SortCompetition();
         int[] randIntArr = randomIntsArr(10000);
         String[] randomStringArr = randomStringArr(10000, 5);
@@ -9,10 +29,14 @@ public class TestRunner
         printArr(randIntArr);
 
         long time = System.currentTimeMillis();
-        int median = team11.challengeOne(randIntArr);\
+        int median = team11.challengeOne(randIntArr);
+        time = System.currentTimeMillis() - time;
+        System.out.println("Challenge One Time Taken: " + time * 0.001 + "Seconds");
+        System.out.println("Median equals: " + median);
 
+        System.out.println("Sorted");
+        printArr(randIntArr);
 
-        gf
+            }
+        }
 
-    }
-}
