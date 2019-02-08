785d1ace8afee9f1c2ba90a04129268220bec4dd
BT_1N3_06 <arafi6624@bths.edu>
2019-02-08 14:45:45 -0500

    a

--- src/Runner.java
+++ src/Runner.java
@@ -26,6 +26,14 @@ public class Runner {
         return arr;
     }
 
+    public static void printArr(String[] arr){
+        String result = "";
+        for (int i = 0; i<arr.length; i++){
+            result = result + arr[i];
+        }
+        System.out.println(arr);
+    }
+
     public static void main (String[] args) {
         SortCompetition team2 = new Team2SortCompetition();
         int[] randomIntsArr = randomIntsArr(10000);
