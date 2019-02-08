f6c19c926f2f8402d95eba7cf7a992c81a1b2f87
BT_1N3_27 <23UI>
2019-02-08 14:32:17 -0500

    Runner finshed test the random string

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -1,10 +1,10 @@
 public class TestRunner {
-    public static String[] randomStringArr(int num,int length) {
+    public static String[] randomStringArr(int num, int length) {
         String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
         String[] arr = new String[length];
-        for(int i = 0; i < length;i++){
-            for(int w = 1; w < num;w++){
-                arr[i]+= abc.charAt(((int)(Math.random()*length*10)));
+        for (int i = 0; i < length; i++) {
+            for (int w = 1; w < num; w++) {
+                arr[i] += abc.charAt(((int) (Math.random() * length * 10)));
             }
 
         }
@@ -14,24 +14,24 @@ public class TestRunner {
     }
 
     public static int[] randomIntsArr(int length) {
-        int [] arr = new int[length];
-        for(int w = 0; w < arr.length;w++){
-            arr[w] = (int)(Math.random()*100000);
+        int[] arr = new int[length];
+        for (int w = 0; w < arr.length; w++) {
+            arr[w] = (int) (Math.random() * 100000);
         }
         return arr;
     }
 
-    public static void printArr(int [] arr) {
+    public static void printArr(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-                System.out.print(arr[i]);
+            System.out.print(arr[i]);
 
         }
         System.out.println();
     }
 
-    public static void main (String[]args){
+    public static void main(String[] args) {
         SortCompetition team11 = new Team13SortCompetition();
-        int[] randIntArr = randomIntsArr(10000);
+        int[] randIntArr = {1,23,2344,56565,86,9,0,45657578,22,1,343,2,563,6655,9244,8566};
         String[] randomStringArr = randomStringArr(10000, 5);
 
         System.out.println("Unsorted");
@@ -45,7 +45,19 @@ public class TestRunner {
 
         System.out.println("Sorted");
         printArr(randIntArr);
+    }
+}
+
+
+        /*long time = System.currentTimeMillis();
+        int median = team11.challengeOne(randIntArr);
+        time = System.currentTimeMillis() - time;
+        System.out.println("Challenge One Time Taken: " + time * 0.001 + "Seconds");
+        System.out.println("Median equals: " + median);
+
+        System.out.println("Sorted");
+        printArr(randIntArr);
 
             }
-        }
+        }*/
 
