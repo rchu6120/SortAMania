8b0e6e2218b31f263cfac82f14b1735355378f78
BT_1N3_29 <hkaur0745@bths.edu>
2019-02-08 14:26:06 -0500

    uopdategdf

--- src/Team12SortCompetition.java
+++ src/Team12SortCompetition.java
@@ -3,149 +3,114 @@ public class Team12SortCompetition extends SortCompetition {
     public int challengeOne(int[] arr) {
         mergeSort(arr);
         return getMedian(arr);
-        }
+    }
 
-        @Override
-        public int challengeTwo (String[]arr, String query){
+    @Override
+    public int challengeTwo(String[] arr, String query) {
         bubbleSort(arr);
-        return binarySearch(arr,query);
-        }
+        return binarySearch(arr, query);
+    }
 
-        @Override
-        public int challengeThree ( int[] arr){
+    @Override
+    public int challengeThree(int[] arr) {
         insertionSort(arr);
         return getMedian(arr);
-        }
+    }
 
-        @Override
-        public int challengeFour ( int[][] arr){
-            {
-                int[] medval= new int[arr.length];
-                for(int j=0; j<arr.length;j++)
-                {
-                    medval[j]=challengeThree(arr[j]);
-                }
-                for(int i=0;i<medval.length;i++)
-                {
-                    for (int j = i; j > 0; j--)
-                    {
-                        if(medval[j] < medval[j-1])
-                        {
-                            swap(medval,j,j-1);
-                        }
+    @Override
+    public int challengeFour(int[][] arr) {
+        {
+            int[] medval = new int[arr.length];
+            for (int j = 0; j < arr.length; j++) {
+                medval[j] = challengeThree(arr[j]);
+            }
+            for (int i = 0; i < medval.length; i++) {
+                for (int j = i; j > 0; j--) {
+                    if (medval[j] < medval[j - 1]) {
+                        swap(medval, j, j - 1);
                     }
                 }
-                return getMedian(medval);
+            }
+            return getMedian(medval);
+        }
+    }
+
+    @Override
+    public int challengeFive(Comparable[] arr, Comparable query) {
+        mergeSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == query) {
+                return i;
             }
         }
-
-        @Override
-        public int challengeFive (Comparable[]arr, Comparable query) {
-                mergeSort(arr);
-                for(int i=0;i<arr.length;i++)
-                {
-                    if(arr[i]==query)
-                    {
-                        return i;
-                    }
-                }
-                return -1;
-        }
+        return -1;
+    }
 
 
+    @Override
+    public String greeting() {
+        return "Hello! We are team 12 :)";
+    }
+
+    // all algorithms being used above
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        @Override
-        public String greeting () {
-            return "Hello! We are team 12 :)";
-        }
-
-        // all algorithms being used above
-
-
-        public static void merge(int[] elements, int from, int mid, int to, int[] temp){
-            int i = from;
-            int j = mid + 1;
-            int k = from;
-            while (i <= mid && j <= to){
-                if (elements[i] < elements[j]){
-                    temp[k] = elements[i];
-                    i++;
-                }
-                else{
-                    temp[k] = elements[j];
-                    j++;
-                }
-                k++;
-            }
-            while (i <= mid){
+    public static void merge(int[] elements, int from, int mid, int to, int[] temp) {
+        int i = from;
+        int j = mid + 1;
+        int k = from;
+        while (i <= mid && j <= to) {
+            if (elements[i] < elements[j]) {
                 temp[k] = elements[i];
                 i++;
-                k++;
-            }
-            while (j <= to){
+            } else {
                 temp[k] = elements[j];
                 j++;
-                k++;
             }
-            for (k = from; k <= to; k++){
-                elements[k] = temp[k];
-            }
+            k++;
         }
-        public void mergeSortHelper(int[] arr, int left, int right, int[] temp){
-            if (left < right){
-                int mid = (left+right)/2;
-                mergeSortHelper(arr,left, mid,temp);
-                mergeSortHelper(arr,mid+1,right,temp);
-                merge(arr, left, mid, right, temp);
-            }
+        while (i <= mid) {
+            temp[k] = elements[i];
+            i++;
+            k++;
         }
-        public void mergeSort(int[] arr){
-            int n = arr.length;
-            int[] temp = new int[n];
-            mergeSortHelper(arr,0,n-1,temp);
+        while (j <= to) {
+            temp[k] = elements[j];
+            j++;
+            k++;
         }
+        for (k = from; k <= to; k++) {
+            elements[k] = temp[k];
+        }
+    }
+
+    public void mergeSortHelper(int[] arr, int left, int right, int[] temp) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSortHelper(arr, left, mid, temp);
+            mergeSortHelper(arr, mid + 1, right, temp);
+            merge(arr, left, mid, right, temp);
+        }
+    }
+
+    public void mergeSort(int[] arr) {
+        int n = arr.length;
+        int[] temp = new int[n];
+        mergeSortHelper(arr, 0, n - 1, temp);
+    }
 
 
-    public static int getMedian(int [] arr)
-        {
+    public static int getMedian(int[] arr) {
         // to find the median
         int middle = arr.length / 2;
         int median = 0; //declare variable
         if (arr.length % 2 == 1)
-            median = (int)arr[middle];
+            median = (int) arr[middle];
         else
-            median = (int)(arr[middle - 1] + arr[middle]) / 2;
+            median = (int) (arr[middle - 1] + arr[middle]) / 2;
         return median;
     }
+
     public static void bubbleSort(String[] arr) {
         int i;
         boolean check = true;  // will determine when the sort is finished
@@ -311,56 +276,56 @@ public class Team12SortCompetition extends SortCompetition {
             return true;
         }
     }
-    public static boolean checkSum(double [] before, double [] after){
-        if (before.length != after.length){
+
+    public static boolean checkSum(double[] before, double[] after) {
+        if (before.length != after.length) {
             return false;
         }
         double sumBef = 0;
         double sumAft = 0;
-        for (int i =0; i < before.length-1; i++){
+        for (int i = 0; i < before.length - 1; i++) {
             sumBef = sumBef + before[i];
             sumAft = sumAft + after[i];
         }
-        if (sumAft != sumAft){
+        if (sumAft != sumAft) {
             return false;
         } else {
             return true;
         }
     }
-    public static int binarySearch(int[] elements, int target){
+
+    public static int binarySearch(int[] elements, int target) {
         int left = 0;
         int right = elements.length - 1;
-        while (left <= right){
-            int middle = (left + right) /2;
-            if (target < elements[middle]){
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (target < elements[middle]) {
                 right = middle - 1;
-            }
-            else if (target > elements[middle]){
+            } else if (target > elements[middle]) {
                 left = middle + 1;
-            }
-            else {
+            } else {
                 return middle;
             }
         }
         return -1;
     }
-    public static double binarySearch(double[] elements, int target){
+
+    public static double binarySearch(double[] elements, int target) {
         int left = 0;
         int right = elements.length - 1;
-        while (left <= right){
-            int middle = (left + right) /2;
-            if (target < elements[middle]){
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (target < elements[middle]) {
                 right = middle - 1;
-            }
-            else if (target > elements[middle]){
+            } else if (target > elements[middle]) {
                 left = middle + 1;
-            }
-            else {
+            } else {
                 return middle;
             }
         }
         return -1;
     }
+
     public static int binarySearch(String[] elements, String target) {
         int left = 0;
         int right = elements.length - 1;
@@ -377,34 +342,28 @@ public class Team12SortCompetition extends SortCompetition {
         return -1;
     }
 
-    public static void printArr(int [] arr)
-    {
+    public static void printArr(int[] arr) {
         String out = "";
-        for (int i = 0; i < arr.length; i++ )
-        {
+        for (int i = 0; i < arr.length; i++) {
             out = out + "[" + arr[i] + "], ";
         }
-        System.out.println(out+"\n");
+        System.out.println(out + "\n");
     }
 
-    public static void printArr(double [] arr)
-    {
+    public static void printArr(double[] arr) {
         String out = "";
-        for (int i = 0; i < arr.length; i++ )
-        {
+        for (int i = 0; i < arr.length; i++) {
             out = out + "[" + arr[i] + "], ";
         }
-        System.out.println(out+"\n");
+        System.out.println(out + "\n");
     }
 
-    public static void printArr(String [] arr)
-    {
+    public static void printArr(String[] arr) {
         String out = "";
-        for (int i = 0; i < arr.length; i++ )
-        {
+        for (int i = 0; i < arr.length; i++) {
             out = out + "[" + arr[i] + "], ";
         }
-        System.out.println(out+"\n");
+        System.out.println(out + "\n");
     }
 
     public static Comparable[] mergeComparable(Comparable[] list1, Comparable[] list2) {
@@ -432,33 +391,47 @@ public class Team12SortCompetition extends SortCompetition {
 
     }
 
-    public static Comparable[] mergeSort(Comparable[] list)
-    {
-        if (list.length <= 1)
-        {
-            return list;
-        }
-        else
-        {
-            int x=list.length/2; // middle
-            Comparable[] temp1 = Arrays.copyOfRange(list,0,x);
-            Comparable[] temp2 = Arrays.copyOfRange(list,x,list.length);
-            return mergeComparable(mergeSort(temp1),mergeSort(temp2));
-        }
+    public void mergeSort(Comparable[] arr) {
+        int n = arr.length;
+        Comparable[] temp = new Comparable[n];
+        mergeSortHelper(arr, 0, n - 1, temp);
     }
 
-    public static Comparable[] mergeSortHelper(int[] arr, int left, int right, int[] temp){
-        if (left < right){
-            int mid = (left+right)/2;
-            mergeSortHelper(arr,left, mid,temp);
-            mergeSortHelper(arr,mid+1,right,temp);
+    public void mergeSortHelper(Comparable[] arr, int left, int right, Comparable[] temp) {
+        if (left > right) {
+            int mid = (left + right) / 2;
+            mergeSortHelper(arr, left, mid, temp);
+            mergeSortHelper(arr, mid + 1, right, temp);
             merge(arr, left, mid, right, temp);
         }
     }
-    public void mergeSort(int[] arr){
-        int n = arr.length;
-        int[] temp = new int[n];
-        mergeSortHelper(arr,0,n-1,temp);
+
+    public void merge(Comparable[] elements, int from, int mid, int to, Comparable[] temp) {
+        int i = from;
+        int j = mid + 1;
+        int k = from;
+        while (i <= mid && j <= to) {
+            if (elements[i].compareTo(elements[j]) < 0) {
+                temp[k] = elements[i];
+                i++;
+            } else {
+                temp[k] = elements[j];
+                j++;
+            }
+            k++;
+        }
+        while (i <= mid) {
+            temp[k] = elements[i];
+            i++;
+            k++;
+        }
+        while (j <= to) {
+            temp[k] = elements[j];
+            j++;
+            k++;
+        }
+        for (k = from; k <= to; k++) {
+            elements[k] = temp[k];
+        }
     }
 }
-
