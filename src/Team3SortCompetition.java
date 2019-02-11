027b04bd54140cdb7f5f2fe69ffee3b963da2e8b
danazargarova <dzargarova6105@bths.edu>
2019-02-11 00:35:45 -0500

    hahah finally done great

--- src/Team3SortCompetition.java
+++ src/Team3SortCompetition.java
@@ -77,7 +77,7 @@ public class Team3SortCompetition extends SortCompetition {
         while (swaps>0){
             swaps=0;
             for(int i=0; i<arr.length-1; i++){
-                if(arr[i].compareTo(arr[i+1])<0){
+                if(arr[i].compareTo(arr[i+1])>0){
                     Comparable temp = arr[i];
                     arr[i]= arr[i+1];
                     arr[i+1] = temp;
